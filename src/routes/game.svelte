980aab76e71d065966fc5ce92255b287e8db76b2
elliotcraft79 <16109561+elliotcraft79@users.noreply.github.com>
2020-11-11 18:46:11 +1030

    Update Timer (again)

---
+++ src/routes/game.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Timer, {startTimer, stopTimer} from '../components/Timer.svelte';
+</script>
+<Timer/>
+<button on:click={startTimer}>
+	start
+</button>
+<button on:click={stopTimer}>
+	stop
+</button>
